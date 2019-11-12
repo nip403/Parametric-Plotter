7483d6c1fed71115fc64cfcf52ad528396471fc2
nip403 <41174986+nip403@users.noreply.github.com>
2019-11-12 23:00:01 +0000

    Update demo.py

--- demo.py
+++ demo.py
@@ -6,15 +6,13 @@ def x(t):
 def y(t):
     return np.sin(t)*50
 
-def main():
-    g = Grapher(winsize=500, axis=500)
-    
+def main(g):
     g.set_time_constraints(t0=0, step=0.02, fps=50)
     g.init_x_eq(x)
     g.init_y_eq(y)
 
-    #g.begin(tstop=300, loop=False)
     g.begin()
 
 if __name__ == "__main__":
-    main()
+    g = Grapher(winsize=500, axis=500)
+    main(g)
