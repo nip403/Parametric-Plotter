bce49110a5bfb836ac903cde08c5bb31df602553
nip403 <41174986+nip403@users.noreply.github.com>
2019-11-24 16:47:18 +0000

    Created lightweight method of drawing graph

--- engine.py
+++ engine.py
@@ -12,7 +12,7 @@ class Grapher:
         self.grapher = _graphics(self.surf, self.axis)
         self.clock = pygame.time.Clock()
 
-        self.surf.fill((255,255,255))
+        self.surf.fill((255, 255, 255))
         pygame.display.flip()
 
     def set_time_constraints(self, t0, step, fps): # add params stop, loop
@@ -28,11 +28,10 @@ class Grapher:
         self.y = y
 
     def begin(self):
-        points = []
+        point0 = self.grapher.translate(self.x(self.t), self.y(self.t))
+        self.t += self.ts
 
-        for i in range(2):
-            points.append(self.grapher.translate(self.x(self.t), self.y(self.t)))
-            self.t += self.ts
+        self.grapher.draw_axes()
 
         while True:
             pygame.display.set_caption(f"t = {round(self.t, 2)}")
@@ -42,9 +41,14 @@ class Grapher:
                 if event.type == pygame.QUIT:
                     pygame.quit()
 
-            points.append(self.grapher.translate(self.x(self.t), self.y(self.t)))
-            self.grapher.plot(points)
-            self.t += self.ts
+            if self.t <= self.axis//2:
+                point1 = self.grapher.translate(self.x(self.t), self.y(self.t))
+                pygame.draw.aaline(self.surf, (0, 0, 255), point0, point1)
+
+                self.t += self.ts
+                point0 = point1
+
+            pygame.display.flip()
 
 class _graphics:
     def __init__(self, surf, axis):
@@ -56,22 +60,17 @@ class _graphics:
     def draw_axes(self):
         self.surf.fill((255, 255, 255))
         
-        for p, i in enumerate(range(0, self.axis, self.axis//self.s * 10)):
+        for p, i in enumerate(range(0, self.axis+1, self.axis//self.s * 10)):
             relative = int(i - self.axis/2)
 
-            if not p % 5:
+            if not p % 5 and relative:
                 num = self.f.render(str(relative), True, (255, 0, 0))
-                self.surf.blit(num, num.get_rect(center=[self.s/2 + 10, i * self.axis/self.s]))
-                self.surf.blit(num, num.get_rect(center=[i * self.axis/self.s, self.s/2 + 10]))
+                self.surf.blit(num, num.get_rect(center=[self.s/2 + 20, self.axis - (i * self.axis/self.s)]))
+                self.surf.blit(num, num.get_rect(center=[i * self.axis/self.s, self.s/2 + 20]))
             
             if not p % 5:
                 pygame.draw.line(self.surf, (0, 0, 0), (0, i * self.axis/self.s), (self.s, i * self.axis/self.s), 4 if not relative else 1)
                 pygame.draw.line(self.surf, (0, 0, 0), (i * self.axis/self.s, 0), (i * self.axis/self.s, self.s), 4 if not relative else 1)
 
-    def plot(self, points):
-        self.draw_axes()
-        pygame.draw.aalines(self.surf, (0, 0, 255), False, points)
-        pygame.display.flip()
-
     def translate(self, x, y):
-        return x + self.axis/2, self.axis/2 - y
+        return x + (self.axis)/2, self.axis/2 - y # clarify scale adjustment where axis != screensize***
